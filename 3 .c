f7a2d3fa4decbdc636d5a34b31a3a0279dfbf529
Tushar Jogani <163539198+tushar6009@users.noreply.github.com>
2024-03-23 17:21:14 +0530

    Add files via upload

---
+++ 3 .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main(){
+	
+	int a, b;
+	
+	printf("Enter your first number:\n");
+	scanf("%d", &a);
+	printf("Enter your second number:\n");
+	scanf("%d", &b);
+	
+	
+	printf("%d * %d = %d", a, b, a*b);
+}
